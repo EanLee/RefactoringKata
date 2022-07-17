6a5153b129b7b51e29a69da11c6b96f628e3fe34
Ean <wosilee@gmail.com>
2022-07-17 22:26:54 +0800

    Added: 增加練習範例

--- CSharp/Practice/LabIntroduceParameter.cs
+++ CSharp/Practice/LabIntroduceParameter.cs
@@ -4,6 +4,14 @@
     {
         public void Main()
         {
+            var result = Pow();
+        }
+
+        private static double Pow()
+        {
+            var value = 3.2;
+
+            return value * value;
         }
     }
 }
--- CSharp/Practice/LabRename.cs
+++ CSharp/Practice/LabRename.cs
@@ -4,6 +4,17 @@
     {
         public void Main()
         {
+            var aaaa = 1;
+
+            for (var i = 0; i < 10; i++)
+            {
+                if (aaaa == 5)
+                {
+                    continue;
+                }
+
+                aaaa++;
+            }
         }
     }
 }
